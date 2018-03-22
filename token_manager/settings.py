becde7905c3222c2711aa95bd0c04435422687d8
Marcus Oliveira <killertiger@gmail.com>
2018-03-21 23:00:58 -0300

    Heroku configuration

--- token_manager/settings.py
+++ token_manager/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
