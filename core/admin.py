25a13801e94ccda307447ed42b0ac43382e1ae80
Marcus Oliveira <killertiger@gmail.com>
2018-03-21 22:32:42 -0300

    Initial

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from core.models import CodeGenerated
+
+admin.site.register(CodeGenerated)
---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class CodeGenerated(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    used_at = models.DateTimeField(null=True)
+    code = models.CharField(max_length=10)
+    metadata = models.TextField(max_length=1000)
